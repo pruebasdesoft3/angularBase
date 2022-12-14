d4ea2e073914a19b7e14d4bb72be56c2482f9d1f
victorPrueba <pruebasdesoft3@gmail.com>
2022-12-13 21:50:51 -0500

    proyecto de dbz sin implementación de service

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Injectable()
+export class DbzService {
+
+  personajes : Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ]
+
+
+	constructor() {
+			console.log('Servicio dbxService');
+	}
+}
